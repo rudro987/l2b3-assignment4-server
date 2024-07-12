bc5be759af0de87c849d1313598205da4c79d9b1
Tanvir Rudro <rudro987@gmail.com>
2024-07-12 22:31:32 +0600

    products modules created and interface declared

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-// import { ProductsRoutes } from './modules/products/products.route'
-// import { OrdersRoutes } from './modules/orders/orders.route'
 
 const app: Application = express()
 
@@ -9,10 +7,6 @@ const app: Application = express()
 
 app.use(express.json())
 app.use(cors())
-
-// //routes
-// app.use('/api/products', ProductsRoutes)
-// app.use('/api/orders', OrdersRoutes)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Appolo level-2 assignment 4 server!')
---
+++ src/modules/products/products.interface.ts
@@ -0,0 +1,10 @@
+export type TProducts = {
+  name: string
+  description: string
+  image: string
+  brand: string
+  price: number
+  rating: number
+  quantity: number
+  isDeleted: boolean
+}
